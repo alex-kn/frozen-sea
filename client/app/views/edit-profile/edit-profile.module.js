856efc6965e9f982759f5b13de05f5484e981b08
Fabian <plettfabian@gmail.com>
2017-01-23 09:18:12 +0100

    Reworked /edit-profile

--- client/app/views/edit-profile/edit-profile.module.js
+++ client/app/views/edit-profile/edit-profile.module.js
@@ -8,115 +8,78 @@ angular.module('editProfile', ['ngRoute', 'ngMaterial'])
             controller: 'editProfileController'
         });
     }])
-    .controller('editProfileController', ['$scope', 'Subuser','$mdDialog','$window', function ($scope, User, $mdDialog, $window) {
+    .controller('editProfileController', ['$scope', 'Subuser','$mdDialog','$route', function ($scope, User, $mdDialog, $route) {
         $scope.title = 'Profil bearbeiten';
-        $scope.currentUserData = {
-            username:'',
-            //email:'',
-            name:'',
-            password:''
+        $scope.input = {};
+        $scope.error = {};
+        $scope.toggleActive = {
+            Username: false,
+            Passwort: false
         };
-
+        $scope.currentUserData = {
+            Username:'',
+            //Email:'',
+            //Name:'',
+            Passwort:''
+        };
         $scope.userId = '';
+
         User.getCurrent(function(userData){
             console.log(userData);
-            $scope.currentUserData.username = userData.username;
-            //$scope.currentUserData.email = userData.email;
-            $scope.currentUserData.name = userData.name;
+            $scope.currentUserData.Username = userData.username;
+            //$scope.currentUserData.Email = userData.email;
+            //$scope.currentUserData.Name = userData.name;
             $scope.userId = userData.id;
         });
 
-        $scope.showPrompt = function(ev, key){
-            if (key == 'password'){
-                $scope.showPromptPassword();
-            }
-            if (key == 'username'){
-                $scope.showPromptUsername();
-            }
-            if (key == 'email'){
-                $scope.showPromptEmail();
-            }
-            if (key == 'name'){
-                $scope.showPromptName();
-            }
+        $scope.toggleArrow = function (key) {
+            $scope.toggleActive[key] = !$scope.toggleActive[key];
         };
 
         $scope.showPromptPassword = function(ev) {
-            var confirm = $mdDialog.prompt()
-                .title('Passwort ändern')
-                .placeholder('Neues Passwort')
-                .ariaLabel('Password')
-                .targetEvent(ev)
-                .ok('Ändern')
-                .cancel('Abbrechen');
-
-            $mdDialog.show(confirm).then(function(result) {
-                User.prototype$updateAttributes({ id: $scope.userId }, {'password':result})
-                    .$promise.then(function (){
-                    $window.location.reload();
-                });
-            }, function() {
-                // Abbrechen
+            var parentEl = angular.element(document.body);
+            $mdDialog.show({
+                controller: editProfileTest,
+                parent: parentEl,
+                targetEvent: ev,
+                templateURL:'index.html'
             });
+
         };
 
-        $scope.showPromptUsername = function(ev, test) {
-            var confirm = $mdDialog.prompt()
-                .title('Username ändern')
-                .placeholder($scope.currentUserData.username)
-                .ariaLabel('username')
-                .initialValue($scope.currentUserData.username)
-                .targetEvent(ev)
-                .ok('Ändern')
-                .cancel('Abbrechen');
-
-            $mdDialog.show(confirm).then(function(result) {
-                User.prototype$updateAttributes({ id: $scope.userId }, {'username':result})
-                    .$promise.then(function (){
-                    $window.location.reload();
-                });
-            }, function() {
-                // Abbrechen
-            });
+        $scope.changeUsername = function(){
+            User.prototype$updateAttributes({ id: $scope.userId }, {'username': $scope.input.name}, function(){
+                $route.reload();
+            },function(err) {
+                if (err.data.error.status == 422) {
+                    $scope.error.username ='Username existiert bereits.';
+                } else {
+                    $scope.error.username = err.data.error.message;
+                }
+            })
         };
 
-        $scope.showPromptEmail = function(ev) {
-            var confirm = $mdDialog.prompt()
-                .title('Email ändern')
-                .placeholder($scope.currentUserData.email)
-                .ariaLabel('email')
-                .initialValue($scope.currentUserData.email)
-                .targetEvent(ev)
-                .ok('Ändern')
-                .cancel('Abbrechen');
+        $scope.changePassword = function() {
+            var hasPassword = false;
+            var comparePassword = $scope.input.newPassword == $scope.input.repeatNewPassword;
 
-            $mdDialog.show(confirm).then(function(result) {
-                User.prototype$updateAttributes({ id: $scope.userId }, {'email':result})
-                    .$promise.then(function (){
-                    $window.location.reload();
-                });
+            if(!comparePassword){
+                $scope.error.newPassword = 'Neues Passwort stimmt nicht überein.';
+            }
+
+            User.login({username: $scope.currentUserData.Username, password: $scope.input.oldPassword}, function(){
+                hasPassword = true;
             }, function() {
-                // Abbrechen
+                $scope.error.oldPassword = 'Altes Passwort stimmt nicht überein.';
             });
+
+            if (hasPassword && comparePassword) {
+                User.prototype$updateAttributes({id: $scope.userId}, {'password': $scope.input.newPassword}, function () {
+                    $route.reload();
+                }, function (err) {
+                    $scope.error = err.data.error.message;
+                })
+            }
         };
 
-        $scope.showPromptName = function(ev, test) {
-            var confirm = $mdDialog.prompt()
-                .title('Namen ändern')
-                .placeholder($scope.currentUserData.name)
-                .ariaLabel('name')
-                .initialValue($scope.currentUserData.name)
-                .targetEvent(ev)
-                .ok('Ändern')
-                .cancel('Abbrechen');
-
-            $mdDialog.show(confirm).then(function(result) {
-                User.prototype$updateAttributes({ id: $scope.userId }, {'name':result})
-                    .$promise.then(function (){
-                    $window.location.reload();
-                });
-            }, function() {
-                // Abbrechen
-            });
-        };
     }]);
