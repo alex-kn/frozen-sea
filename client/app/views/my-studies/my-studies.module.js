fcbe304b6dbe27c74cd9e56fa31ffcae4c80c977
Fabian <plettfabian@gmail.com>
2017-02-03 08:38:47 +0100

    Reworked /my-studies

--- client/app/views/my-studies/my-studies.module.js
+++ client/app/views/my-studies/my-studies.module.js
@@ -1,30 +1,57 @@
 angular
     .module('myStudies', ['ngRoute', 'ngMaterial'])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/my-studies', {
-            templateUrl: 'views/my-studies/my-studies.template.html',
-            controller: 'MyStudiesController'
-        });
-    }])
-    .controller('MyStudiesController', ['$scope', 'Study', '$mdDialog', 'Participation', '$location', 'Subuser', 'LoopBackAuth',
-        function ($scope, Study, $mdDialog, Participation, $location, Subuser, LoopBackAuth) {
+    .controller('MyStudiesController', ['$scope', 'Study', 'Participation', 'Subuser', 'LoopBackAuth',
+        function ($scope, Study, Participation, Subuser, LoopBackAuth) {
 
             $scope.title = 'Meine Studien';
-            $scope.studies = Participation.find({
+            $scope.myStudies = [];
+
+            Participation.find({
                 filter: {
                     where: {
                         subuserId: LoopBackAuth.currentUserId
                     }
                 }
+            }, function(userParticipations){
+                Study.find(function(allStudies){
+                    for(var i = 0; i < userParticipations.length; i++) {
+                        for (var j = 0; j < allStudies.length; j++) {
+                            if (allStudies[j].id == userParticipations[i].studyId) {
+                                $scope.myStudies.push(allStudies[j]);
+                            }
+                        }
+                    }
+                });
+
+
+                /* Query for every StudyId
+
+                 var myStudies = [];
+
+                 for(i = 0; i < result.length; i++){
+                 $scope.studies = Study.find({
+                 filter: {
+                 where: {
+                 id: result[i].studyId
+                 }
+                 }
+                 }, function(result){
+                 console.log(result);
+                 var resultLength = result.length;
+                 for(j = 0; j < resultLength; j++){
+
+                 myStudies.push(result[j]);
+                 myStudies = myStudies.filter(function( item, index, inputArray ) {
+                 return inputArray.indexOf(item) == index;
+                 });
+
+                 $scope.studies = myStudies;
+
+                 }
+                 });
+                 }
+                 */
+
             });
 
-            console.log($scope.studies);
-
-
-
-
-
-
-
         }]);
-
