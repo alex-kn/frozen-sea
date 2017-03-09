2d4c71a1875d7870c4b9da2694649ffbf0082277
Mathis <mathis.gales@gmail.com>
2017-03-09 10:44:07 +0100

    pre-filter studies adapted to new models owner now always sees his own studies consistent use of terms lefty/righty

--- client/app/filters/studies.filter.js
+++ client/app/filters/studies.filter.js
@@ -7,48 +7,110 @@ angular.module('userStudy').filter('filterStudies', function() {
     return function(input, userPreferences, optional2) {
         var out = [];
 
+        function _calculateAge(birthday) { // birthday is a date
+            var ageDifMs = Date.now() - new Date(birthday).getTime();
+            var ageDate = new Date(ageDifMs); // miliseconds from epoch
+            console.log(ageDate.getUTCFullYear() - 1970);
+            return Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
+
+
         angular.forEach(input, function (study) {
 
-            //console.log(typeof study.age_requirement_min_years == 'undefined' || (study.age_requirement_min_years < userPreferences.age)); //old enough
-            //console.log(typeof study.age_requirement_max_years == 'undefined' ||(study.age_requirement_max_years < userPreferences.age)); //not too old
-            //console.log(typeof study.height_requirement_min_cm == 'undefined' ||(study.height_requirement_min_cm < userPreferences.height)); //tall enough
-            //console.log(typeof study.height_requirement_max_cm == 'undefined' ||(study.height_requirement_max_cm < userPreferences.age)); //not too tall
-            //console.log((study.requires_ios_experience || userPreferences.ios) || !study.requires_ios_experience); //knows ios
-            //console.log((study.requires_excellent_vision || !userPreferences.glasses) || !study.requires_excellent_vision);
-            //console.log((study.requires_glasses || userPreferences.glasses) || !study.requires_glasses);
-            //console.log((study.requires_glasses || userPreferences.glasses) || !study.requires_glasses);
-            //console.log((study.requires_english_skills || userPreferences.english) || !study.requires_english_skills);
-            //console.log((study.requires_german_skills || userPreferences.german) || !study.requires_german_skills);
-            //console.log((study.requires_lefty || userPreferences.lefty) || !study.requires_lefty);
-            //console.log((study.requires_righty || userPreferences.righty) || !study.requires_righty);
-            //console.log((study.requires_student || userPreferences.student) || !study.requires_student);
-            //console.log((study.requires_non_student || !userPreferences.student) || !study.requires_non_student);
 
-            //console.log((typeof study.requires_study_programme_comma_seperated == 'undefined'
-            //    || study.requires_study_programme_comma_seperated.split(',').contains(userPreferences.study_program)));
-
-            //console.log((study.requires_only_male_participants || !userPreferences.gender === "male") || !study.requires_only_male_participants);
-            //console.log((study.requires_only_female_participants || !userPreferences.gender === "female") || !study.requires_only_female_participants);
+            var isMatch = (typeof study.minimum_age == 'undefined'
+                    || typeof userPreferences.birthDate == 'undefined'
+                    || userPreferences.birthDate == '1970-01-01T00:00:00.000Z' //workaround.
+                    || study.minimum_age < _calculateAge(userPreferences.birthDate)) //old enough
 
 
-            var isMatch = (typeof study.age_requirement_min_years == 'undefined' || study.age_requirement_min_years < userPreferences.age) //old enough
-                && (typeof study.age_requirement_max_years == 'undefined' || study.age_requirement_max_years < userPreferences.age) //not too old
-                && (typeof study.height_requirement_min_cm == 'undefined' || study.height_requirement_min_cm < userPreferences.height) //tall enough
-                && (typeof study.height_requirement_max_cm == 'undefined' || study.height_requirement_max_cm < userPreferences.age) //not too tall
-                && ((study.requires_ios_experience && userPreferences.ios) || !study.requires_ios_experience) //knows ios
-                && ((study.requires_excellent_vision && !userPreferences.glasses) || !study.requires_excellent_vision)
-                && ((study.requires_glasses && userPreferences.glasses) || !study.requires_glasses)
-                && ((study.requires_glasses && userPreferences.glasses) || !study.requires_glasses)
-                && ((study.requires_english_skills && userPreferences.english) || !study.requires_english_skills)
-                && ((study.requires_german_skills && userPreferences.german) || !study.requires_german_skills)
-                && ((study.requires_lefty && userPreferences.lefty) || !study.requires_lefty)
-                && ((study.requires_righty && userPreferences.righty) || !study.requires_righty)
-                && ((study.requires_student && userPreferences.student) || !study.requires_student)
-                && ((study.requires_non_student && !userPreferences.student) || !study.requires_non_student)
-                && ((typeof study.requires_study_programme_comma_seperated == 'undefined'
-                || study.requires_study_programme_comma_seperated.split(',').contains(userPreferences.study_program)))
-                && (typeof userPreferences.gender == 'undefined' ||(study.requires_only_male_participants && userPreferences.gender.male) || !study.requires_only_male_participants)
-                && (typeof userPreferences.gender == 'undefined' ||(study.requires_only_female_participants && userPreferences.gender.female) || !study.requires_only_female_participants);
+                && (typeof study.minimum_height == 'undefined'
+                    ||typeof userPreferences.height == 'undefined'
+                    || study.minimum_height < userPreferences.height) //tall enough
+
+
+                && (typeof study.operatingSystem_required == 'undefined'
+                || typeof userPreferences.operatingSystem == 'undefined'
+                    || (study.operatingSystem_required.ios && userPreferences.operatingSystem.ios)
+                    || !study.operatingSystem_required.ios) //knows ios (undefined defaults to false)
+
+
+                && (typeof study.operatingSystem_required == 'undefined'
+                || typeof userPreferences.operatingSystem == 'undefined'
+                || (study.operatingSystem_required.android && userPreferences.operatingSystem.android)
+                || !study.operatingSystem_required.android) //knows android (undefined defaults to false)
+
+
+                && (typeof study.operatingSystem_required == 'undefined'
+                || typeof userPreferences.operatingSystem == 'undefined'
+                || (study.operatingSystem_required.windows && userPreferences.operatingSystem.windows)
+                || !study.operatingSystem_required.windows) //knows android (undefined defaults to false)
+
+
+                && (typeof study.visualAid_required == 'undefined'
+                || typeof userPreferences.visualAid == 'undefined'
+                || (study.visualAid_required.glasses && userPreferences.visualAid.glasses)
+                || !study.visualAid_required.glasses)
+
+
+                && (typeof study.visualAid_required == 'undefined'
+                || typeof userPreferences.visualAid == 'undefined'
+                || (study.visualAid_required.contactLenses && userPreferences.visualAid.contactLenses)
+                || !study.visualAid_required.contactLenses)
+
+
+                && (typeof study.visualAid_required == 'undefined'
+                || typeof userPreferences.visualAid == 'undefined'
+                || (study.visualAid_required.none && userPreferences.visualAid.none)
+                || !study.visualAid_required.none)
+
+
+                && (typeof study.language_required == 'undefined'
+                || typeof userPreferences.language == 'undefined'
+                || (study.language_required.english && userPreferences.language.english)
+                || !study.language_required.english)
+
+
+                && (typeof study.language_required == 'undefined'
+                || typeof userPreferences.language == 'undefined'
+                || (study.language_required.german && userPreferences.language.german)
+                || !study.language_required.german)
+
+
+                && ((study.required_handedness == 'lefty' && (userPreferences.handedness == 'lefty' || userPreferences.handedness == 'both'))
+                || !(study.required_handedness == 'lefty'))
+
+
+                && ((study.required_handedness == 'righty' && (userPreferences.handedness == 'righty'|| userPreferences.handedness == 'both'))
+                || !(study.required_handedness == 'righty'))
+
+
+                && (typeof study.required_study_programs_array == 'undefined'
+                || (study.required_study_programs_array.length > 0 && userPreferences.student)
+                || !(study.required_study_programs_array.length > 0))
+
+
+                && (typeof study.required_study_programs_array == 'undefined'
+                || (typeof study.course == 'undefined'
+                || study.required_study_programs_array.indexOf(userPreferences.course)>-1))
+
+
+                && (typeof userPreferences.gender == 'undefined'
+                || typeof study.required_gender == 'undefined'
+                || (study.required_gender.male && userPreferences.gender == 'male')
+                || !study.required_gender.male)
+
+
+                && (typeof userPreferences.gender == 'undefined'
+                || typeof study.required_gender == 'undefined'
+                ||(study.required_gender.female && userPreferences.gender == 'female')
+                || !study.required_gender.female);
+
+
+            //The owner should always see his own studies
+            if(study.ownerId == userPreferences.subuserId) {
+                isMatch = true;
+            }
 
 
             if (isMatch) {
