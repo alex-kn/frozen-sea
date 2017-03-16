edb3c214fdf9291880112b9d28aa1e19ccf152d5
Mathis <mathis.gales@gmail.com>
2017-03-16 17:04:03 +0100

    unique filter sort fix 3

--- client/app/components/study-list/study-list.directive.js
+++ client/app/components/study-list/study-list.directive.js
@@ -15,9 +15,9 @@ angular
                 $scope.studyIsReLoading = false;
                 $scope.studies = [];
                 $scope.thereAreMatchingStudies = true;
-                $scope.show_too_old = false;
-                $scope.show_non_matches = false;
-                $scope.sort_by =  "ends_soon"; //default sort value
+                $scope.show_too_old = true;
+                $scope.show_non_matches = true;
+                $scope.sort_by =  "newest"; //default sort value
                 $scope.searchGtXs = false;
                 $scope.search = false;
                 $scope.filter = false;
@@ -74,8 +74,11 @@ angular
             };
             $scope.loadStudies = function() {
                 $scope.myFilter = {};
-                if(!$scope.show_too_old) { //load all studies that are not finished yet
+                if($scope.show_too_old) { //load all studies that are not finished yet
                     $scope.myFilter = {filter: {where: {endDate:  {gte: new Date()}}}};
+                    console.log("NEW ONLY");
+                } else {
+                    console.log("OLD");
                 }
                 $scope.studiesTemp = Study.find($scope.myFilter,
                     function(list) {
@@ -84,11 +87,13 @@ angular
                 );
             };
             $scope.refilter = function() {
+                $scope.studies = $scope.studiesTemp;
+
                 if ($scope.show_non_matches) {
-                    //filter all studies that don't match user profile
-                    $scope.studies = $filter('filterStudies')($scope.studiesTemp, $scope.preferences);
+                    $scope.studies = $filter('filterStudies')($scope.studies, $scope.preferences);
+                    console.log("MATCHES ONLY");
                 } else {
-                    $scope.studies = $scope.studiesTemp;
+                    console.log("NON MATCHES");
                 }
                 $scope.thereAreMatchingStudies = $scope.studies.length > 0;
 
---
+++ client/app/filters/unique.filter.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Mathis on 16.03.2017.
+ */
+angular.module('userStudy').filter('unique', function() {
+    return function(collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if(keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+
+        return output;
+    };
+});
